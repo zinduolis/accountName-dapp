81335cab214f96dd4f73986fbba9b679f57bcdae
Your Name <you@example.com>
2022-09-12 09:05:16 +1000

    added creating contact

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Web3 from 'web3';
 import Contacts from './artifacts/Contacts.json';
 
@@ -7,7 +7,6 @@ const contactsAddress = "0xCdb0B80B74C62d329f7B0b344f05E48ebdf0DC36";
 function App() {
   const [account, setAccount] = useState();
   const [counter, setCounter] = useState();
-  // const [contacts, setContacts] = useState();
 
   async function fetchContacts() {
     const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
@@ -16,14 +15,12 @@ function App() {
     const contactsContract = new web3.eth.Contract(Contacts.abi, contactsAddress);
     const counter = await contactsContract.methods.count.call().call();
     console.log('counter: ', counter);
-    const contactList = await contactsContract.methods.contacts(accounts[0]).call();
-   // console.log('contact list: ', contactList);
+    let contactList = await contactsContract.methods.contacts(accounts[0]).call();
+    console.log('contact list Before: ', contactList);
+    contactList = await contactsContract.methods.createContact('Someone', '235423523').send({from: accounts[0]});
+    console.log('contact list After: ', contactList);
     setCounter(counter);
   }
-
-  useEffect(() => {
-    console.log("Counter has changed")
-  },[counter])
 
   return (
     <div className="App">
