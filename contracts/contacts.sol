ccb82098451b3e432badd79f7333819df3348d82
Your Name <you@example.com>
2022-09-12 11:01:07 +1000

    updated to set accounts name

--- contracts/contacts.sol
+++ contracts/contacts.sol
@@ -1,23 +1,19 @@
 //SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >0.4.17 <0.9.0;
 
 contract Contacts {
-    uint256 public count = 0; // state variable
 
-    mapping(uint => Contact) public contacts;
-
-    struct Contact {
-        uint id;
-        string name;
-        string phone;
-    }
+    mapping(address => string) public Name;
 
     constructor() public {
-        createContact('Red Graz', '134424');
+        setName(msg.sender, 'Red Graz');
     }
 
-    function createContact(string memory _name, string memory _phone) public {
-        count++;
-        contacts[count] = Contact(count, _name, _phone);
+    function retrieveName(address _owner) public view returns (string memory) {
+        return Name[_owner];
+    }
+
+    function setName(address _owner, string memory _name) public {
+        Name[_owner]= _name;
     }
 }
--- src/App.js
+++ src/App.js
@@ -2,36 +2,48 @@ import { useState } from 'react';
 import Web3 from 'web3';
 import Contacts from './artifacts/Contacts.json';
 
-const contactsAddress = "0xCdb0B80B74C62d329f7B0b344f05E48ebdf0DC36";
+const contactAddress = "0x4a12809239c2134265809e36a9B2309201213E6d";
 
 function App() {
   const [account, setAccount] = useState();
-  const [counter, setCounter] = useState();
+  const [name, setName] = useState();
+  let accounts;
 
-  async function fetchContacts() {
+  async function fetchAccount() { 
     const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
-    const accounts = await web3.eth.requestAccounts();
+    accounts = await web3.eth.requestAccounts();
     setAccount(accounts[0]);
-    const contactsContract = new web3.eth.Contract(Contacts.abi, contactsAddress);
-    const counter = await contactsContract.methods.count.call().call();
-    console.log('counter: ', counter);
-    let contactList = await contactsContract.methods.contacts(accounts[0]).call();
-    console.log('contact list Before: ', contactList);
-    contactList = await contactsContract.methods.createContact('Someone', '235423523').send({from: accounts[0]});
-    console.log('contact list After: ', contactList);
-    setCounter(counter);
+    console.log(account);
+  }
+  
+  async function getName() {
+    await fetchAccount();
+    const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+    const contactsContract = new web3.eth.Contract(Contacts.abi, contactAddress);
+    const whatsName = await contactsContract.methods.retrieveName(accounts[0]).call();
+    console.log('Name of the account is: ', whatsName);
+    setName(whatsName);
+  }
+
+  async function writeName() {
+    await fetchAccount();
+    const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+    const contactContract = new web3.eth.Contract(Contacts.abi, contactAddress);
+    await contactContract.methods.setName(accounts[0], name).send({from: accounts[0]});    
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={fetchContacts}>Fetch Contacts</button>
+        <button onClick={getName}>What is my account's name?</button>
       </header>
       <h5 className="strong">
         <strong>Your account is: {account}</strong>
-        <p>This is how many contacts you have: {counter}</p>
-
+        <p>Your account's name is: {name}</p>
       </h5>
+      <button onClick={writeName}>Change Account Name</button>
+      <input onChange={e => setName(e.target.value)} placeholder="new name..." />
+
     </div>
   );
 }
